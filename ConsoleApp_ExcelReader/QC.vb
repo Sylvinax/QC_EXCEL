02cff03eaf6e3262dbf3caa2d99125bf8f07d182
Sylvinax <sylvinax@gmail.com>
2017-07-07 01:19:02 +0800

    d

--- ConsoleApp_ExcelReader/QC.vb
+++ ConsoleApp_ExcelReader/QC.vb
@@ -8,11 +8,24 @@ Public Class QC
         Return objTest
     End Function
 
+    Function Add_Test(path)
+
+
+
+
+
+
+
+
+
+    End Function
+
     Sub WriteTestSteps(test As Test, source As String(,))
         Dim objDesignFactory As DesignStepFactory
         Dim objDesignStep As DesignStep
         objDesignFactory = test.DesignStepFactory
-        objDesignStep = objDesignFactory.AddItem("Step1")
+        objDesignStep = objDesignFactory.AddItem(vbNull)
+        objDesignStep.StepName = "Step1"
         objDesignStep.StepDescription = "StepDescription"
         objDesignStep.StepExpectedResult = "StepExpectedResult"
         ' ArrayList SSS
